a2e18db6969602831f96d8ad401c6361a87f22b0
roberson.faria <putz280raf>
2016-12-26 12:27:25 -0200

    add menssagem de erro para formato de cep

--- config/config.php
+++ config/config.php
@@ -4,10 +4,12 @@ return [
         'cns' => 'O campo :attribute é inválido.',
         'cnpj' => 'O campo :attribute é inválido.',
         'cpf' => 'O campo :attribute é inválido.',
+        'cep_format' => 'O campo :attribute não possui um formato de cep válido',
     ],
     'en' => [
         'cns ' => 'The field :attribute is not valid',
         'cnpj ' => 'The field :attribute is not valid',
         'cpf' => 'The field :attribute is not valid',
+        'cep_format' => 'The field :attribute does not have a valid zip format',
     ]
 ];
